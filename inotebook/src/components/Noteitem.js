1a9d705aad6168df2eeee94521c99e8d8c8dc901
Shayan <shayanlost1@gmail.com>
2022-08-20 01:53:50 +0500

    note card update

--- inotebook/src/components/Noteitem.js
+++ inotebook/src/components/Noteitem.js
@@ -6,8 +6,8 @@ const Noteitem = (props) => {
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
-          <p className="card-text">{note.description}</p>
+          <h5 className="card-title"> {note.title} </h5>
+          <p className="card-text"> {note.description} </p>
         </div>
       </div>
     </div>
