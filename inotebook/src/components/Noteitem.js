a07805a9ed51312dbb6e93f9fe8f2b73ab155ddc
Shayan <shayanlost1@gmail.com>
2022-08-25 01:35:57 +0500

    add font-awesom icons and alert

--- inotebook/src/components/Noteitem.js
+++ inotebook/src/components/Noteitem.js
@@ -3,10 +3,14 @@ import React from "react";
 const Noteitem = (props) => {
   const { note } = props;
   return (
-    <div className="col-md-3">
+    <div className="col-md-4">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title"> {note.title} </h5>
+          <div className="d-flex align-items-center">
+          <h5 className="card-title "> {note.title} </h5>
+          <i class="fa-regular fa-pen-to-square mx-2"></i>
+          <i class="fa-regular fa-trash-can"></i>
+          </div>
           <p className="card-text"> {note.description} </p>
         </div>
       </div>
